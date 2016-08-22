b42ba94ab518844a2df71023263708368a38e267
Jena SanCartier <jenasancartier@gmail.com>
2016-08-22 15:57:06 -0700

    spit back inputted data in a ul

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,20 @@ function ToDo(task, location, details, priority) {
   this.priority = priority;
 }
 
-
 //user interface logic
+$(document).ready(function(){
+  $("form#new-todo").submit(function(event){
+    event.preventDefault();
+
+    var inputtedTask = $("input#new-task").val();
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedDetails = $("input#new-details").val();
+    var inputtedPriority = $("input#priority").val();
+
+    var newToDo = new ToDo(inputtedTask, inputtedLocation, inputtedDetails, inputtedPriority);
+
+    $("ul#tasks").append("<li><span class='task'>" + newToDo.task + "</span></li>");
+
+
+  });
+});
