34c237ee9fb565a8be8e563cc97b086d4de7c2b1
Jena SanCartier <jenasancartier@gmail.com>
2016-08-22 15:42:22 -0700

    create constructor in business logic portion of scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//business logic
+function ToDo(task, location, details, priority) {
+  this.task = task;
+  this.location = location;
+  this.details = details;
+  this.priority = priority;
+}
+
+
+//user interface logic
